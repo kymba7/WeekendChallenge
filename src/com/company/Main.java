e38fcf7b2ab3ff26767a2f51f1ad7de0e9870ee5
Kymbrlee <kymba7@gmail.com>
2018-01-16 17:57:14 -0700

    Day6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,35 +25,73 @@ public class Main {
                 System.out.println("Please enter the next number for your calculation");
                 String userinput3 = robo.nextLine();
 
-                int firstN = Integer.parseInt(userinput);
-                int thirdN = Integer.parseInt(userinput3);
-                int finalResult = 0;
+                int firstI = Integer.parseInt(userinput);
+                int thirdI = Integer.parseInt(userinput3);
+//                int finalResult = 0;
+//                public static int add(int firstN, int thirdN){
+//                    int newSum = firstN + thirdN;
+//                    System.out.println("The sum is ");
+//                    return newSum;
+//                }
+//                public static int sub ( int firstN, thirdN){
+//                    int newSum = firstN - thirdN;
+//                    System.out.println("The difference is ");
+//                }
+//                public static int mult ( int firstN, thirdN){
+//                    int newSum = firstN * thirdN;
+//                    System.out.println("the product is ");
+//                }
+//                public static int div ( int firstN, thirdN){
+//                    int newSum = firstN / thirdN;
+//                    System.out.println("the quotient is ");
 
 
-                if (userinput2.equals("+")) {
-                    finalResult = firstN + thirdN;
-                } else if (userinput2.equals("-")) {
-                    finalResult = firstN - thirdN;
-                } else if (userinput2.equals("*")) {
-                    finalResult = firstN * thirdN;
-                } else if (userinput2.equals("/")) {
-                    finalResult = firstN / thirdN;
-                }
-
+//                if (userinput2.equals("+")) {
+//                    finalResult = firstN + thirdN;
+//                } else if (userinput2.equals("-")) {
+//                    finalResult = firstN - thirdN;
+//                } else if (userinput2.equals("*")) {
+//                    finalResult = firstN * thirdN;
+//                } else if (userinput2.equals("/")) {
+//                    finalResult = firstN / thirdN;
+//                }
+            int finalResult = doMath(firstI, thirdI, userinput2);
 
                 System.out.println("Here is your final number: " + finalResult);
 
 
-            } else{
+            } else {
                 ask = false;
-            System.out.println("Goodbye");
+                System.out.println("Goodbye");
 
-        }}
-    }}
+            }
+        }
+    } //end of static main
+
+    public static int doMath(int firstN, int thirdN, String op) {
+        int newSum = 0;
+
+        if (op.equals("+")) {
+            newSum = firstN + thirdN;
+        } else if (op.equals("-")) {
+            newSum = firstN - thirdN;
+        } else if (op.equals("*")) {
+            newSum = firstN * thirdN;
+        } else if (op.equals("/")) {
+            newSum = firstN / thirdN;
+        }
+        ;
+
+        return newSum;
+    }
+}
 
 
 
-       // System.out.println("Pick which function you would like to use: Add, Subtract, Multiply or Divide");
+
+
+
+// System.out.println("Pick which function you would like to use: Add, Subtract, Multiply or Divide");
         //String[]prompt = {"Please select a function: Add, Subtract, Multiply or Divide."};
        // String[] userInformation = ("");
 
